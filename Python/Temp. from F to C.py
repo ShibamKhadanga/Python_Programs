d3dbc361ff4f2920cc0e7e089839a85690de8117
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-01-18 11:53:48 +0530

    Add files via upload

---
+++ Python/Temp. from F to C.py
@@ -0,0 +1,6 @@
+#Temperature Conversion from fahrenheit to Clsius
+
+f=int(input("Enter temp. in fahrenheit: "))
+c=(f-32)/1.8
+print("Temp in  celsius is: ", c)
+print(f"fahrenheit = {f} into celsius = {c}")
