77a9d972a4fcdc57332a0622031f42709766f71e
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-01-28 21:39:24 +0530

    Update Armstrong number.py

--- Python/Armstrong number.py
+++ Python/Armstrong number.py
@@ -1,17 +1,19 @@
+#...........................................Checking the Armstrong No. using While Loop
+
 n = int(input("Enter a Number: "))
 c = 0
 s = 0
 i = n
 
 while i > 0:
-    c+=1
+    c+=1          #Counting No. of digit 
     i = i//10
     
 i = n
 while i > 0:
     r = i%10
-    s = s +(r**c)
-    i = i//10
+    s = s +(r**c)         # sum of each digit to the power of no. of digit
+    i = i//10             # 153 = 1^3 + 5^3 + 3^3 = 153
 
 if s == n:
     print("The given No. is a Armstrong number")
