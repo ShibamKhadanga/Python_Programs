00889bc2ed8843a52ee0dfa01d06cd11d075a3c8
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-02-01 16:59:21 +0530

    Add files via upload

---
+++ Python/Print with condition.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    print('Weird' if n % 2 != 0 or (6 <= n <= 20) else 'Not Weird')
