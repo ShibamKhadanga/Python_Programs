d4f82da6450c2ae6fbb47170f553dc10c689c22f
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-02-25 21:46:26 +0530

    Add files via upload

---
+++ Python/Grading Students.py
@@ -0,0 +1,63 @@
+'''HackerLand University has the following grading policy:
+
+* Every student receives a grade in the inclusive range from 0 to 100.
+* Any grade less than 40 is a failing grade.
+
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+* If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+* If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+Examples:-
+
+* grade = 84 round to 85 (85 - 84 is less than 3)
+* grade = 29 do not round (result is less than 38)
+* grade = 57 do not round (60 - 57 is 3 or higher)
+
+Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    round_grades=[]
+    for i in grades:
+        if i<38:
+            round_grades.append(i)
+        elif (i+(5-i%5))-i < 3:
+            x=i+(5-i%5)
+            round_grades.append(x)
+        else:
+            round_grades.append(i)
+    return round_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
