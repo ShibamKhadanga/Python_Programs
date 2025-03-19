ee13fc166777c15529020d19941e535444d9d033
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-03-20 01:20:53 +0530

    Update Insertion Sort.py

--- Python/Insertion Sort.py
+++ Python/Insertion Sort.py
@@ -4,7 +4,8 @@
 ''' .................................................Insertion Sort............................................... '''
 
 # l = list(map(int, input("Enter Sace Separated Numbers: ").split()))
-# l=[12,10,35,2,21,32]
+
+l=[12,10,35,2,21,32]
 
 
 print('Given List of no. is: \t',l)
@@ -14,12 +15,14 @@ for i in range(1,len(l)):
     print('\n\nStep:',i,'--->',l)
     
     for j in range (0,i):
-        print('\n',l[i],'<',l[j],'is ', l[i]<l[j])
+        print('\n\t',l[i],'<',l[j],'is ', l[i]<l[j])
+        print('\t
+              Swap',l[i],'with',l[j])
         
         if l[i]<l[j]:
             l[i],l[j]=l[j],l[i]
             
-        print(l)
+        print('\t',l)
     print('\n')
 
 print('Sorter List of no. is: \t',l)
