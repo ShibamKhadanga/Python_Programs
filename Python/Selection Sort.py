64b21482b5fd19ca4f651c6772ef90d9227a988f
Shibam Khadanga <shibamkhadanga947@gmail.com>
2025-03-21 20:58:29 +0530

    Update Selection Sort.py

--- Python/Selection Sort.py
+++ Python/Selection Sort.py
@@ -3,27 +3,25 @@
 
 ''' ................................................. Selection Sort............................................... '''
 
-# l = list(map(int, input("Enter Sace Separated Numbers: ").split()))
-l=[12,10,35,2,21,32]
+l = list(map(int, input("Enter Sace Separated Numbers: ").split()))
+# l=[12,10,35,2,21,32]
 
 
 print('Given List of no. is: \t',l)
 
 
-for i in range(len(l)-1):
+for i in range(len(l)):
     print('\n\nStep:',i+1,'--->',l)
 
-    mini = l[i]
+    mini = i
     for j in range(i+1,len(l)):
         
-        if l[j]<mini:
-            mini=l[j]
+        if l[j]<l[mini]:
+            mini=j
     print(f"\n\t\tMinimum: {mini}")
     print(f'\t\tSwapping {mini} with {l[i]}(FOR INDEX-{i})')
 
-    x=l.index(mini)
-    if l[x] < l[i]:
-        l[x],l[i]=l[i],l[x]
+    l[i],l[mini]=l[mini],l[i]
     print( '\n\t    ',l)
 
 print('\n\nSorter List of no. is: \t',l)
